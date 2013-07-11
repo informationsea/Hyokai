e53975a3a371d23c2daa9f3ff7092525c2ff967c
Y.Okamura <okamura@informationsea.info>
2013-07-11 13:56:51 +0900

    update buildscript for mac

--- scripts/script-for-mac/build.sh
+++ scripts/script-for-mac/build.sh
@@ -1,21 +1,44 @@
 #!/bin/sh
 
+DISTDIR="Table View"
+APPNAME="TableView.app"
+
+
 if [ -f Makefile ];then
     make distclean
 fi
-qmake -spec macx-g++ -config release ../../src
+
+if [ -d "$DISTDIR" ];then
+    rm -r "$DISTDIR"
+fi
+
+qmake -spec macx-clang -config release ../../src
 make -j5
-macdeployqt TableView.app # for dynamic link library
+echo "Deploying..."
+macdeployqt $APPNAME # for dynamic link library
 #strip TableView.app/Contents/MacOS/TableView # for static link library
 #cp -R qt_menu.nib TableView.app/Contents/Resources
-cp Info.plist TableView.app/Contents/
-cp ../../src/images/fileicon/sqlite.icns TableView.app/Contents/Resources
-cp ../../src/images/fileicon/table.icns TableView.app/Contents/Resources
+cp Info.plist $APPNAME/Contents/
+cp ../../src/images/fileicon/sqlite.icns $APPNAME/Contents/Resources
+cp ../../src/images/fileicon/table.icns $APPNAME/Contents/Resources
 
 if [ -f ./codesign.sh ];then
     sh ./codesign.sh
 fi
 
-rm TableView-osx.zip
-7za a TableView-osx.zip TableView.app
+mkdir "$DISTDIR"
+
+mv "$APPNAME" "$DISTDIR/$APPNAME"
+cp ../../COPYING "$DISTDIR/LICENSE.txt"
+cp ../../README.md "$DISTDIR/README.md"
+cp ../../sampledata/iris.data.sqlite3 "$DISTDIR/sample-iris.sqlite3"
+ln -s /Applications "$DISTDIR/Applications"
+
+echo "Creating disk image"
+hdiutil create -ov -srcfolder "$DISTDIR" -fs HFS+J -format UDBZ -volname "Table View" tableview.dmg
+
+#rm TableView-osx.zip
+#7za a TableView-osx.zip TableView.app
+
+
 
