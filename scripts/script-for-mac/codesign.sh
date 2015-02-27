993ec95b61656c4c7c7dc27b51b978067499d9ca
OKAMURA Yasunobu <okamura@informationsea.info>
2015-02-27 22:53:15 +0900

    update codesign.sh to support Qt 5.4

--- scripts/script-for-mac/codesign.sh
+++ scripts/script-for-mac/codesign.sh
@@ -14,21 +14,6 @@ fi
 
 APPNAME=Hyokai.app
 
-for i in `find ${APPNAME}/Contents/Frameworks -name '*.framework'`; do
-    BASENAME="${i##*/}"
-
-    pushd $i
-    rmdir Resources
-    mkdir -p Versions/5/Resources
-    ln -s Versions/5/Resources .
-    pushd Versions
-    ln -s 5 Current
-    popd
-    cp ${QTPATH}/lib/$BASENAME/Contents/Info.plist Versions/5/Resources
-    popd
-    
-done
-
 find ${APPNAME} -type f -perm 755 -exec codesign --force --verify --verbose -s "$CODE_SIGN_IDENTITY" '{}' ';'
 find ${APPNAME} -name '*.framework' -exec codesign --force --verify --verbose -s "$CODE_SIGN_IDENTITY" '{}' ';'
 codesign --force --verify --verbose -s "$CODE_SIGN_IDENTITY" ${APPNAME}
