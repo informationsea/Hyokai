8526f60574d94c838acc26a055e712fcd3c39419
OKAMURA Yasunobu <okamura@informationsea.info>
2015-12-07 15:33:20 +0900

    Fix installer build script

--- scripts/script-for-windows/installer/buildinstaller.bat
+++ scripts/script-for-windows/installer/buildinstaller.bat
@@ -1,2 +1,11 @@
 candle HyokaiInstaller.wxs
+if not "%ERRORLEVEL%"  == "0" GOTO ERR
+
 light -ext WixUIExtension HyokaiInstaller.wixobj
+if not "%ERRORLEVEL%"  == "0" GOTO ERR
+
+EXIT 0
+
+:ERR
+@echo "Failed to build"
+EXIT 1
