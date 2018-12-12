51ac3bc7c68cca6fd00c44b40c661cb82249e4e0
Yasunobu Okamura <okamura@informationsea.info>
2018-12-13 00:08:16 +0900

    add windows build scripts

---
+++ scripts/script-for-windows/build_vs2015-qt5.9.7-32.bat
@@ -0,0 +1,5 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
+set PATH=C:\Qt\5.9.7\msvc2015\bin;%PATH%;C:\Program Files (x86)\Windows Kits\8.1\bin\x86
+echo %PATH%
+build.bat
+
