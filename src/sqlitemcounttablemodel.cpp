32bc9bae7f7133c9ba1acfc8a4d5c067454b8c73
OKAMURA Yasunobu <okamura@informationsea.info>
2019-01-17 17:07:44 +0900

    show ratio in summary view

--- src/sqlitemcounttablemodel.cpp
+++ src/sqlitemcounttablemodel.cpp
@@ -1,4 +1,6 @@
 #include "sqlitemcounttablemodel.h"
+#include <QDebug>
+#include <QSqlError>
 
 SqlItemCountTableModel::SqlItemCountTableModel(QSqlDatabase *database, QString fromValue, QString columnName, QString where, QObject *parent)
 	: QSqlQueryModel(parent), m_database(database), m_fromValue(fromValue), m_columnName(columnName), m_where(where)
@@ -25,11 +27,11 @@ void SqlItemCountTableModel::sort(int column, Qt::SortOrder order)
 
 	// Summary
 	if (m_where.isEmpty()) {
-		m_query = QSqlQuery(QString("SELECT \"%1\", count(*) as count FROM %2 GROUP BY \"%1\" ORDER BY %3 %4").
+        m_query = QSqlQuery(QString("SELECT *, 1.0*item_count/(SELECT count(*) FROM %2) ratio FROM (SELECT \"%1\", count(*) as item_count FROM %2 GROUP BY \"%1\")  ORDER BY %3 %4").
 			arg(m_columnName, m_fromValue, QString::number(column + 1), orderStr), *m_database);
 	}
 	else {
-        m_query = QSqlQuery(QString("SELECT \"%1\", count(*) as count FROM %2 WHERE %3 GROUP BY \"%1\" ORDER BY %4 %5").
+        m_query = QSqlQuery(QString("SELECT *, 1.0*item_count/(SELECT count(*) FROM %2) ratio FROM (SELECT \"%1\", count(*) as item_count FROM %2 WHERE %3 GROUP BY \"%1\") ORDER BY %4 %5").
 			arg(m_columnName, m_fromValue, m_where, QString::number(column + 1), orderStr), *m_database);
 	}
 	this->setQuery(m_query);
